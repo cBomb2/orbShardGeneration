df7f4d792df68e22a444e0f9100806e4aa5877b3
learning.amplify.com <100149163+cBomb2@users.noreply.github.com>
2024-10-22 16:08:08 -0700

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,5 @@ if(orbType = 1){
   document.getElementById("a").href = "watabou.github.io/one-page-dungeon/?seed=" + seed;
   document.getElementById("ifr").src = "watabou.github.io/one-page-dungeon/?seed=" + seed;
 } else {
-  document.getElementById("a").innerText = "entity";
-  document.getElementById("ifr").src="github.com/zachreborn/dnd_5e_encounter_gen.git";
+  document.getElementById("a").innerText = "entity"
 }
