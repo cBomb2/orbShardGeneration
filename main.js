e119cf11e45bceb2d3ed260508a083e0c6477124
learning.amplify.com <100149163+cBomb2@users.noreply.github.com>
2024-10-23 15:00:45 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const orbType = Math.round(Math.random() * 2);
 if(orbType = 1){
   document.getElementById("a").innerText = "dungeon";
   const seed = Math.round(Math.random() * 1000000000);
-  document.getElementById("a").href = "watabou.github.io/one-page-dungeon/?seed=" + seed;
-  document.getElementById("ifr").src = "watabou.github.io/one-page-dungeon/?seed=" + seed;
+  document.getElementById("a").href = "watabou.github.io/one-page-dungeon/?seed=" + window.location.search.split(8);
+  document.getElementById("ifr").src = "watabou.github.io/one-page-dungeon/?seed=" + window.location.search.split(8);
 } else {
   document.getElementById("a").innerText = "entity" + encounter;
 }
